ebd987c954c4a94bb0deafeae5f89ef538184656
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-22 18:19:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,6 @@
 def introduction_with_language(name,language)
   
+<<<<<<< HEAD
   puts"Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
@@ -9,3 +10,7 @@ def introduction(name)
 end  
   
   
+=======
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+>>>>>>> 1585d16abd3f96ce42724b3063a17bbab93f6b74
